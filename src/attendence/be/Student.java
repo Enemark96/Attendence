60cd67d146c77b19e61b5166fa0a795d30a4390d
JamesBabz <web.fuhlendorff@gmail.com>
2017-02-09 11:54:55 +0100

    mockDate
    mock data with getter

--- src/attendence/be/Student.java
+++ src/attendence/be/Student.java
@@ -11,4 +11,74 @@ package attendence.be;
  */
 public class Student {
     
+    int id;
+    String firstName;
+    String lastName;
+    String username;
+    String password;
+    String className;
+
+    public Student(int id, String firstName, String lastName, String username, String password, String className)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.className = className;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getClassName()
+    {
+        return className;
+    }
+
+    public void setClassName(String className)
+    {
+        this.className = className;
+    }
+    
+    
+    
+    
 }
