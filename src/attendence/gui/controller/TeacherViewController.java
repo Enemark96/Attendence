135aa5d39033e0edd400a4a4ba5aee54c3558a33
Enemark96 <je.enemark96@gmail.com>
2017-02-09 10:39:51 +0100

    Controller fix
    Controller fix

--- src/attendence/gui/controller/TeacherViewController.java
+++ src/attendence/gui/controller/TeacherViewController.java
@@ -7,7 +7,9 @@ package attendence.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -15,6 +17,9 @@ import javafx.fxml.Initializable;
  * @author Jacob Enemark
  */
 public class TeacherViewController implements Initializable {
+
+    @FXML
+    private Label lblUsername;
 
     /**
      * Initializes the controller class.
