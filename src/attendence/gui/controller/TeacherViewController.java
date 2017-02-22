a2c1298c42250f9a622feb2377ff9ce5126aada1
Enemark96 <je.enemark96@gmail.com>
2017-02-22 14:30:05 +0100

    CSS CHANGES I UI
    CSS CHANGES I UI

--- src/attendence/gui/controller/TeacherViewController.java
+++ src/attendence/gui/controller/TeacherViewController.java
@@ -17,10 +17,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -44,6 +47,8 @@ public class TeacherViewController implements Initializable {
     List<Student> studentList;
     List<Absence> absenceList;
     ObservableList<Student> allStudents;
+    @FXML
+    private Button closeButton;
     
 
     public TeacherViewController()
@@ -79,5 +84,12 @@ public class TeacherViewController implements Initializable {
         }
         colAbsence.setCellValueFactory(new PropertyValueFactory<>("amountOfAbsence"));
     }    
+
+    @FXML
+    private void closeWindow(MouseEvent event) {
+          Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+
+    }
     
 }
