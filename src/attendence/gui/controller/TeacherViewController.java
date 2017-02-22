aba83134985c22df034a06d5b2ac32535805303f
JamesBabz <web.fuhlendorff@gmail.com>
2017-02-22 12:31:46 +0100

    teacherview
    teacherview has been updated to show students and their total absence

--- src/attendence/gui/controller/TeacherViewController.java
+++ src/attendence/gui/controller/TeacherViewController.java
@@ -5,12 +5,22 @@
  */
 package attendence.gui.controller;
 
+import attendence.be.Absence;
+import attendence.be.Student;
+import attendence.bll.PersonManager;
 import attendence.gui.model.TeacherModel;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -20,13 +30,30 @@ import javafx.scene.control.Label;
 public class TeacherViewController implements Initializable {
 
     TeacherModel model;
+    PersonManager manager;
     
     @FXML
     private Label lblUsername;
+    @FXML
+    private TableView<Student> tblStudentAbs;
+    @FXML
+    private TableColumn<Student, String> colStudent;
+    @FXML
+    private TableColumn<Student, String> colAbsence;
+    
+    List<Student> studentList;
+    List<Absence> absenceList;
+    ObservableList<Student> allStudents;
+    
 
     public TeacherViewController()
     {
+        this.manager = new PersonManager();
+        this.studentList = manager.getAllStudents();
+        this.absenceList = manager.getAllAbsence();
+        this.allStudents = FXCollections.observableArrayList();
         this.model = TeacherModel.getInstance();
+        allStudents.addAll(studentList);
     }
 
     /**
@@ -36,6 +63,21 @@ public class TeacherViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         lblUsername.setText(model.getCurrentUser().getFirstName()+" "+model.getCurrentUser().getLastName());
+        tblStudentAbs.setItems(allStudents);
+        colStudent.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        for (Student student : studentList)
+        {
+            int x = 0;
+            for (Absence absence : absenceList)
+            {
+                if(student.getId() == absence.getStudentId())
+                {
+                    x++;
+                }
+            }
+            student.setAmountOfAbsence(x);
+        }
+        colAbsence.setCellValueFactory(new PropertyValueFactory<>("amountOfAbsence"));
     }    
     
 }
